da37ab2b577f57b15fb8a6f0a86e6901e8e8a2dc
Artem Sokolov <the.jelll.2006@gmail.com>
2025-07-11 21:18:40 +0500

    v2.4

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -16,6 +16,7 @@ const Button = (props) => {
     // before | after
     iconPosition = "before",
     hasFillIcon,
+    extraAttrs,
   } = props
 
   const isLink = href !== undefined
@@ -36,6 +37,7 @@ const Button = (props) => {
       title={title}
       aria-label={title}
       {...specificProps}
+      {...extraAttrs}
     >
       {iconPosition === "before" && iconComponent}
       {!isLabelHidden && (
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -3,15 +3,30 @@ import "./Slider.scss"
 import CategoryCard from "../CategoryCard"
 import SliderNavigation from "./components/SliderNavigation"
 
+  const defaultsSliderParams = {
+    slidesPerView: 5,
+    slidesPerGroup: 5,
+    spaceBetween: 30,
+  }
+
 const Slider = (props) => {
     const {
         children,
         navigationTargetElementId = null,
+        sliderParams = defaultsSliderParams,
     } = props
 
   return (
-    <div className="slider">
-      <div className="slider__swiper swiper">
+    <div 
+    className="slider" 
+    data-js-slider={JSON.stringify({
+      sliderParams,
+      navigationTargetElementId,
+    })}
+    >
+      <div 
+      className="slider__swiper swiper" 
+      data-js-slider-swiper="">
         <ul className="slider__list swiper-wrapper">
           {children.map((slide, index) => (
             <li className="slider__item swiper-slide" key={index}>
