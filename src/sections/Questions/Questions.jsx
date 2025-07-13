dbfa6fea9e7f220a3e71848e6ec5442f6f23e1c3
Artem Sokolov <the.jelll.2006@gmail.com>
2025-07-13 20:32:08 +0500

    v2.7

--- src/sections/Questions/Questions.jsx
+++ src/sections/Questions/Questions.jsx
@@ -1,3 +1,4 @@
+import Accordion from "@/components/Accordion"
 import AccordionGroup from "@/components/AccordionGroup"
 import Button from "@/components/Button"
 import Section from "@/layouts/Section"
@@ -22,7 +23,17 @@ const Questions = () =>{
             actions={(<Button label="Ask a Question" href="/support" />)}
         >
             <AccordionGroup columns={2}>
-                {questionItems}
+                {questionItems.map((question, index) => (
+                    <Accordion
+                    title={question}
+                    id={`question-${index}`}
+                    name="questions"
+                    isOpen={index === 0}
+                    key={index}
+                    >
+                        <p>StreamVibe is a streaming service that allows you to watch movies and shows on demand.</p>
+                    </Accordion>
+                ))}
             </AccordionGroup>
         </Section>
     )
