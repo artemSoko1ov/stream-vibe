ecff868644eabd743a5968f66646550c08fa5604
Artem Sokolov <the.jelll.2006@gmail.com>
2025-08-02 12:23:28 +0500

    v5.0.1

--- src/components/Field/Field.jsx
+++ src/components/Field/Field.jsx
@@ -1,47 +1,52 @@
-import getIdFromTitle from '@/utils/getIdFromTitle'
 import './Field.scss'
+import getIdFromTitle from '@/utils/getIdFromTitle'
 import classNames from 'classnames'
 
 const Field = (props) => {
-    const {
-        className,
-        id = getIdFromTitle(props.label),
-        label,
-        /**
-         * undefined (default) | 'email' | 'textarea'
-         */
-        type,
-        placeholder,
-        isRequired,
-    } = props
+  const {
+    className,
+    id = getIdFromTitle(props.label),
+    label,
+    /**
+     * undefined (default) | 'email | 'textarea'
+     */
+    type,
+    placeholder,
+    isRequired,
+    inputMode,
+  } = props
 
-    const Component = type === 'textarea' ? 'textarea' : 'input'
+  const Component = type === 'textarea'
+    ? 'textarea'
+    : 'input'
 
-    return (
-       <div
-       className={classNames(className, 'field')}
-       >
-        <label className="field__label" htmlFor={id}
-        >
-            {label} {isRequired && (
-                <span 
-                className="field__required-star"
-                aria-hidden="true"
-                >*</span>
-            )}
-            <div className="field__body"
-            >
-                <Component 
-                    className='field__control'
-                    id={id}
-                    type={type}
-                    placeholder={placeholder}
-                    required={isRequired}
-                />
-            </div>
-        </label>
-       </div>
-    )
+  return (
+    <div
+      className={classNames(className, 'field')}
+    >
+      <label
+        className="field__label"
+        htmlFor={id}
+      >
+        {label} {isRequired && (
+          <span
+            className="field__required-star"
+            aria-hidden="true"
+          >*</span>
+        )}
+      </label>
+      <div className="field__body">
+        <Component
+          className="field__control"
+          id={id}
+          type={type}
+          placeholder={placeholder}
+          required={isRequired}
+          inputMode={inputMode}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Field
