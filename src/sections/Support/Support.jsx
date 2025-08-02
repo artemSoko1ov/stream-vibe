92c79d74e3327ddcd9b42838819bf9d3c73435ec
Artem Sokolov <the.jelll.2006@gmail.com>
2025-08-02 16:50:16 +0500

    v5.3

--- src/sections/Support/Support.jsx
+++ src/sections/Support/Support.jsx
@@ -1,24 +1,25 @@
-import './Support.scss'
-import { Image } from 'minista'
-import Field from '@/components/Field'
-import CheckBox from '@/components/CheckBox'
-import Button from '@/components/Button'
+import "./Support.scss"
+import { Image } from "minista"
+import Field from "@/components/Field"
+import CheckBox from "@/components/CheckBox"
+import Button from "@/components/Button"
+import Select from "@/components/Select"
 
 const Support = () => {
-  const titleId = 'support-title'
+  const titleId = "support-title"
 
   return (
-    <section
-      className="support container"
-      aria-labelledby={titleId}
-    >
+    <section className="support container" aria-labelledby={titleId}>
       <div className="support__body">
         <div className="support__info">
           <h1 className="support__title h2" id={titleId}>
             Welcome to our support page!
           </h1>
           <div className="support__description">
-            <p>We're here to help you with any problems you may be having with our product.</p>
+            <p>
+              We're here to help you with any problems you may be having with
+              our product.
+            </p>
           </div>
         </div>
         <Image
@@ -50,7 +51,20 @@ const Support = () => {
           label="Phone Number"
           placeholder="(999) 999-99-99"
           inputMode="tel"
-          mask='(000) 000-00-00'
+          mask="(000) 000-00-00"
+          renderBefore={(buttonClassName) => (
+            <Select
+              label="Phone number prefix"
+              buttonClassName={buttonClassName}
+              options={[
+                { value: "+7", isSelected: true },
+                { value: "+1" },
+                { value: "+2" },
+                { value: "+3" },
+                { value: "+4" },
+              ]}
+            />
+          )}
         />
         <Field
           className="support__form-cell support__form-cell--wide"
@@ -60,16 +74,16 @@ const Support = () => {
           isRequired
         />
         <div className="support__form-cell support__form-cell--wide support__form-cell--actions">
-            <CheckBox 
-                className="support__form-agreement"
-                label='I agree with Terms of Use and Privacy Policy'
-                isRequired
-            />
-            <Button 
-                className='support__form-submit-button'
-                label='Send Message'
-                type='submit'
-            />
+          <CheckBox
+            className="support__form-agreement"
+            label="I agree with Terms of Use and Privacy Policy"
+            isRequired
+          />
+          <Button
+            className="support__form-submit-button"
+            label="Send Message"
+            type="submit"
+          />
         </div>
       </form>
     </section>
